59220fde2921fddcb3b13eb41e2995ff92ee9038
HeriYantodotDev <yanto.heri@outlook.com>
2023-04-27 14:01:47 +0700

    adding new tag

---
+++ src/hero.js
@@ -0,0 +1,8 @@
+export function hero() {
+  const heroSection = `
+  Hi, my name is <span class="text-color-main">Heri Yanto</span>
+  <br />
+  I'm a Full-stack Developer.
+  `
+  document.getElementById('heroDetail').innerHTML = heroSection;
+}
--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 import { footerYear } from "./footer";
+import { hero } from "./hero";
+import { about } from "./about";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
 
+//adding Hero Detail
+hero();
+//adding About
+about();
 //Adding the recent year to the footage
 footerYear();
